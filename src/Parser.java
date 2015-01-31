a18d9f7a95d85f767a3efed995aaff257e4183ac
NISH1001 <nish_play1001@yahoo.com>
2015-01-31 16:02:44 +0545

    Building generalized

---
+++ src/Parser.java
@@ -0,0 +1,8 @@
+
+public class Parser
+{
+	public Parser()
+	{
+		System.out.println("just a parser...");
+	}
+}
--- src/oata/Test.java
+++
@@ -1,9 +0,0 @@
-package oata;
-
-public class Test 
-{
-    public static void main(String[] args) 
-	{
-        System.out.println("dot dot dot");
-    }
-}
