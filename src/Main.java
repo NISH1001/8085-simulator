0333eed34c196ca7b742387107c9a978e76238fc
NISH1001 <nish_play1001@yahoo.com>
2015-01-31 18:13:13 +0545

    Parser class LOL beginning

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main
 {
     public static void main(String[] args) 
 	{
-		Parser p = new Parser();
+		Parser p = new Parser("test.txt");
+
+		p.DisplayLines();
     }
 }
--- src/Parser.java
+++ src/Parser.java
@@ -1,8 +1,45 @@
+import java.io.*;
+import java.util.*;
 
 public class Parser
 {
-	public Parser()
+	private ArrayList<String> m_lines;
+
+	public Parser(String filename)
 	{
-		System.out.println("just a parser...");
+		m_lines = new ArrayList<String>();
+
+		try
+		{
+    		BufferedReader reader = new BufferedReader( new FileReader( "test.txt"));
+    		String line;
+
+    		while((line = reader.readLine()) != null)
+    		{
+    			//System.out.println(line);
+    			line = line.trim();
+    			if(line.length()>0)
+    			{
+    				line = line.replaceAll("\\s+", " ");
+    				if(line.charAt(0) == ';')
+    					continue;
+    				m_lines.add(line);
+    			}
+    		}
+
+    		reader.close();
+
+		}
+		catch ( IOException e)
+		{
+		}
+	}
+
+	public void DisplayLines()
+	{
+		for(int i=0; i<m_lines.size(); ++i)
+		{
+			System.out.println(m_lines.get(i) + " " + m_lines.get(i).length());
+		}
 	}
 }
