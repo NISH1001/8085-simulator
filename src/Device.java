0878c74c32085058d78d02954ca1e03a69513715
thenav56 <navinayer56@gmail.com>
2015-03-17 18:01:00 +0545

    clean

--- src/Device.java
+++
@@ -1,7 +0,0 @@
-public interface Device {
-
-    public boolean hasAddress(int address);
-    public boolean IO_Mbar();
-    public void writeByte(int address, int data);
-    public byte readByte(int address);
-}
--- src/DeviceException.java
+++
@@ -1,9 +0,0 @@
-public class DeviceException extends Exception {
-    /*public DeviceException() {
-        super();
-    }
-
-    public DeviceException(String message) {
-        super(message);
-    }*/
-}
