0019bdfb11f16bef25de8e10e22752ef2d931333
Neeraj Adhikari <adh.neeraj@gmail.com>
2015-03-22 18:42:17 +0545

    Deleted some files

--- 8085/src/pkg8085/core/Device.java
+++
@@ -1,8 +0,0 @@
-package pkg8085.core;
-
-public interface Device {
-    public boolean hasAddress(int address);
-    public boolean IO_Mbar();
-    public void writeByte(int address, int data);
-    public int readByte(int address);
-}
